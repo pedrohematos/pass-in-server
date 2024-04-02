33c6ff85825c33d447a3497c4ef22f5ccd6cc173
Pedro Matos <pedrohematos@outlook.com>
2024-04-02 19:36:31 +1100

    feat: add fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+import fastify from "fastify";
+
+const port = parseInt(process.env.PORT);
+
+if (isNaN(port)) {
+  console.error("Invalid or missing PORT in environment variables");
+  process.exit(1);
+}
+
+const app = fastify();
+
+app.get("/", () => {
+  return "Hello world!";
+});
+
+app.listen({ port }).then(() => {
+  console.log("HTTP server running on port", port);
+});
